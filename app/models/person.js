f0fccb0d18d1a182947a6342fc30ea12fde6f975
Mark Supalla <marksupalla@gmail.com>
2014-08-17 13:29:34 -0500

    model done

--- app/models/person.js
+++ app/models/person.js
@@ -1,6 +1,14 @@
 'use strict';
 
-function Person(){
+var Mongo = require('mongodb'),
+    _ = require('lodash');
+
+function Person(o){
+  this.name = o.name;
+  this.photo = o.photo;
+  this.cash = parseFloat(o.cash);
+  this.assets = [];
+
 }
 
 Object.defineProperty(Person, 'collection', {
@@ -11,5 +19,30 @@ Person.all = function(cb){
   Person.collection.find().toArray(cb);
 };
 
+Person.prototype.save = function(cb){
+  Person.collection.save(this, cb);
+};
+
+Person.findById = function(id, cb){
+  var _id = Mongo.ObjectID(id);
+  Person.collection.findOne({_id:_id}, function(err, object){
+    var person = changePrototype(object);
+    cb(person);
+  });
+};
+
+Person.prototype.addAsset = function(asset, cb){
+  this.assets.push(asset);
+  Person.collection.update({_id:this._id}, {$push: {assets:asset}}, cb);
+};
+
+
 module.exports = Person;
+
+
+// Private Function //
+
+function changePrototype(object){
+  return _.create(Person.prototype, object);
+}
 
--- test/unit/person.js
+++ test/unit/person.js
@@ -7,7 +7,8 @@ var expect    = require('chai').expect,
     Person    = require('../../app/models/person'),
     dbConnect = require('../../app/lib/mongodb'),
     cp        = require('child_process'),
-    db        = 'template-test';
+    db        = 'asset-lister',
+    Mongo     = require('mongodb');
 
 describe('Person', function(){
   before(function(done){
@@ -26,14 +27,46 @@ describe('Person', function(){
     it('should create a new Person object', function(){
       var p = new Person();
       expect(p).to.be.instanceof(Person);
+      expect(p.name).to.equal('Tom Brady');
+      expect(p.photo).to.equal('http://img2.timeinc.net/people/i/2009/database/tombrady/tom_brady300.jpg');
+      expect(p.cash).to.equal(12000);
+      expect(p.assets).to.have.length(0);
     });
   });
 
   describe('.all', function(){
     it('should get all people', function(done){
       Person.all(function(err, people){
-        expect(people).to.have.length(2);
+        expect(people).to.have.length(3);
         done();
+      });
+    });
+  });
+
+  describe('.findById', function(){
+    it('should find a person by ID', function(){
+      Person.findById('000000000000000000000001', function(person){
+        expect(person._id).to.be.instanceof(Mongo.ObjectID);
+      });
+    });
+  });
+  describe('#save', function(){
+    it('should save a person to the database', function(done){
+      var o = {name:'Kobe Bryant', photo:'http://kobe.img', cash:'1'},
+          p = new Person(o);
+      p.save(function(){
+        expect(p._id).to.be.instanceof(Mongo.ObjectID);
+        done();
+      });
+    });
+  });
+  describe('#addAsset', function(){
+    it('should add an asset to a given person', function(done){
+      Person.findById('000000000000000000000001', function(person){
+        person.addAsset({name: 'BMW', photo: 'http://www.bmw.com/_common/shared/insights/bmw_design_2012/visions/img/bmw-vision-m1-hommage-01.jpg', value:'65000', count:'1'}, function(){
+          expect(person.assets.length).to.equal(3);
+          done();
+        });
       });
     });
   });
